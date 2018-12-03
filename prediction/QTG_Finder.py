178e3c69c6c6c83c05c19ffc90784d27a4fb8471
Fan <flin@ciw.edu>
2018-12-03 09:32:24 -0800

    update

--- prediction/QTG_Finder.py
+++ prediction/QTG_Finder.py
@@ -30,14 +30,12 @@ requiredArg.add_argument('-gl',dest='gl',required=True, help='Input QTL gene lis
 requiredArg.add_argument('-sp',dest='sp',required=True,choices=['AT', 'OS'], help='Species: use "AT" for Arabidopsis; "OS" for rice')
 args = parser.parse_args()
 
-start_time = time.time()
-
 def train_qtg(df, train_set, Validation_set_i):
     if args.sp=='AT': 
         mol_para=['auto',20]# optimum parameters for Arabidopsis
     if args.sp=='OS':
         mol_para=['auto',5]# optimum parameters for rice
-    clf = ensemble.RandomForestClassifier(n_estimators=200, min_samples_split=2,max_features=mol_para[0],n_jobs=None) # Random forest parameters
+    clf = ensemble.RandomForestClassifier(n_estimators=200, min_samples_split=2,max_features=mol_para[0],n_jobs=-1) # Random forest parameters
     neg_inter=5000  # interrations for randomly selecting negatives from genome and re-training models 
     prediction_list = (len(Validation_set_i))*[0]
     for i in range(0, neg_inter):
@@ -66,41 +64,40 @@ def train_qtg(df, train_set, Validation_set_i):
         for i in range(len(rank_list_df_sorted)):            
             indenti_f.write (rank_list_df_sorted['ID'][i]+','+str(rank_list_df_sorted['Rank'][i])+','+ str(rank_list_df_sorted['freq'][i]) +'\n')
 
-dt = args.fl # input feature list
-
 #### check know QTL gene on QTLs, remove, reducant ID run
-
-with open(args.gl,'r') as f:  
-    for key,group in it.groupby(f,lambda line: line.startswith('//') ): # QTLs are seperated by // in input file. 
-        if not key:
-            df = pd.read_csv(dt)
-            df=df.dropna(axis=1,how='all') 
-            df['network_weight'] = preprocessing.scale(df['network_weight']) # standardization
-            group = list(group) # each group is a QTL
-            group=[i.strip('\n') for i in group]
-            QTL_name=group[0]
-            print('QTL: '+QTL_name)
-            genes_in_QTL=group[1:] # gene list
-            original_length= len(genes_in_QTL) # total number of input IDs in a QTL 
-            Validation_set=pd.DataFrame()   
-            for i in range(len(genes_in_QTL)):   
-                Validation_set=Validation_set.append (df[df.ID==genes_in_QTL[i]]) # only keep the gene IDs that can be found in the feature list 
-            df = df.drop(['ID'], axis=1) 
-
-            ind_for_exclusion=[]
-            for t in range(len(Validation_set.index)): # exclude known causal in the QTL regions since these known causal genes are part of the training set 
-                if Validation_set['class'][Validation_set.index[t]]==1:
-                    ind_for_exclusion.append(t)
-                    print ('Known QTL causal gene excluded: ')
-                    print(Validation_set['ID'][Validation_set.index[t]])  
-            Validation_set=Validation_set.drop(Validation_set.index[ind_for_exclusion])  # take out known causal genes from input gene list
-            Validation_set_ID_uni=list(Validation_set.ID) # unique ID, remove redandancy  
-            print('Number of genes: '+str(original_length)  )  # total number of input IDs in a QTL 
-            gene_ex=(set(genes_in_QTL)-set(Validation_set_ID_uni)) # exclude IDs that could not be found in feature list
-            Validation_set=  Validation_set.drop(['ID'], axis=1)  
-            Validation_set_i=Validation_set.reset_index(drop=True) # list of genes to be ranked
-            train_set = df[df['class']==1]# train_set
-            train_qtg(df, train_set, Validation_set_i) # execute 
-
-print("--- %s seconds ---" % round((time.time() - start_time),2) ) 
+if __name__ == '__main__':
+    dt = args.fl # input feature list
+    start_time = time.time()
+    with open(args.gl,'r') as f:  
+        for key,group in it.groupby(f,lambda line: line.startswith('//') ): # QTLs are seperated by // in input file. 
+            if not key:
+                df = pd.read_csv(dt)
+                df=df.dropna(axis=1,how='all') 
+                df['network_weight'] = preprocessing.scale(df['network_weight']) # standardization
+                group = list(group) # each group is a QTL
+                group=[i.strip('\n') for i in group]
+                QTL_name=group[0]
+                print('QTL: '+QTL_name)
+                genes_in_QTL=group[1:] # gene list
+                original_length= len(genes_in_QTL) # total number of input IDs in a QTL 
+                Validation_set=pd.DataFrame()   
+                for i in range(len(genes_in_QTL)):   
+                    Validation_set=Validation_set.append (df[df.ID==genes_in_QTL[i]]) # only keep the gene IDs that can be found in the feature list 
+                
+                df = df.drop(['ID'], axis=1) 
+                ind_for_exclusion=[]
+                for t in range(len(Validation_set.index)): # exclude known causal in the QTL regions since these known causal genes are part of the training set 
+                    if Validation_set['class'][Validation_set.index[t]]==1:
+                        ind_for_exclusion.append(t)
+                        print ('Known QTL causal gene excluded: ')
+                        print(Validation_set['ID'][Validation_set.index[t]])  
+                Validation_set=Validation_set.drop(Validation_set.index[ind_for_exclusion])  # take out known causal genes from input gene list
+                Validation_set_ID_uni=list(Validation_set.ID) # unique ID, remove redandancy  
+                print('Number of genes: '+str(original_length)  )  # total number of input IDs in a QTL 
+                gene_ex=(set(genes_in_QTL)-set(Validation_set_ID_uni)) # exclude IDs that could not be found in feature list
+                Validation_set=  Validation_set.drop(['ID'], axis=1)  
+                Validation_set_i=Validation_set.reset_index(drop=True) # list of genes to be ranked
+                train_set = df[df['class']==1]# train_set
+                train_qtg(df, train_set, Validation_set_i) # execute 
+    print("--- %s seconds ---" % round((time.time() - start_time),2) ) 
 
