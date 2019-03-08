ecca67d1e839d2f314facfdb3a54461250375f77
Fan <flin@ciw.edu>
2019-03-08 14:23:12 -0800

    update

--- tests/AT_lit_validation.sh
+++ tests/AT_lit_validation.sh
@@ -8,11 +8,11 @@
 module purge;
 module load Python/3.6.0 
 
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/Huang_2012.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/Fbien_QTL_genes.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/Motte_2014.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/yuan_2016_XQTL_list.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/Guo_2016_SSQ3_AHK3.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/Guo_2016_SSQ10_AHK2.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/TGG.csv 'AT'
-srun python QTG_literature_validation.py ./input/Arabidopsis_features-v3.05.txt ./input/AT/GSOH1.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/Huang_2012.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/Fbien_QTL_genes.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/Motte_2014.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/yuan_2016_XQTL_list.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/Guo_2016_SSQ3_AHK3.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/Guo_2016_SSQ10_AHK2.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/TGG.csv 'AT'
+srun python QTG_literature_validation.py ./input/Arabidopsis_features_v4.csv./input/AT/GSOH1.csv 'AT'
--- tests/OS_lit_validation.sh
+++ tests/OS_lit_validation.sh
@@ -9,11 +9,11 @@ module purge;
 module load Python/3.6.0 
 
 
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Qi_2008.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Liu_2017.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Hu_2008.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Gao_2016.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Oikawa_2015.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Fan_2016.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/Fukuoka_2014.csv 'OS'
-srun python QTG_literature_validation.py ./input/rice_features_v1.3.11.txt ./input/OS/zeng_2013.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Qi_2008.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Liu_2017.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Hu_2008.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Gao_2016.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Oikawa_2015.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Fan_2016.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/Fukuoka_2014.csv 'OS'
+srun python QTG_literature_validation.py ./input/rice_features_v2.csv ./input/OS/zeng_2013.csv 'OS'
